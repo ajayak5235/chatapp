d1441c83eb524f2992595ba6c4fe5c0587509c48
ajayak5235 <131739918+ajayak5235@users.noreply.github.com>
2023-11-26 18:33:58 +0530

    Update database.js

--- dist/util/database.js
+++ dist/util/database.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('chatapp4', 'root', '12345', {
+const sequelize = new sequelize_1.Sequelize('ch', 'root', '12345896456', {
     dialect: 'mysql',
     host: 'localhost'
 });
